4544ac97b0a8300b88084df04328cc805d71561a
andybao <wenyu.bao@gmail.com>
2018-05-21 16:22:54 -0400

    change FAQs to Faqs

--- faq/faq.php
+++ faq/faq.php
@@ -7,14 +7,14 @@
 ?>
 <main class="no_scroll">
     <section id="faq">
-        <h2 class="text-center">FAQs</h2>
+        <h2 class="text-center">Faqs</h2>
         <div id="accordion">
             <?php foreach ($faqs as $faq): ?>
 
                 <div class="card">
                     <div class="card-header" id="<?= 'faq_heading_' . $faq['id']; ?>">
                         <h5 class="mb-0">
-                            <button class="btn btn-link" data-toggle="collapse" data-target="<?= '#faq_collapse_' . $faq['id']; ?>" aria-expanded="true" aria-controls="<?= 'faq_collapse_' . $faq['id']; ?>">
+                            <button class="btn btn-link" id="<?= 'faq_btn_' . $faq['id']; ?>" data-toggle="collapse" data-target="<?= '#faq_collapse_' . $faq['id']; ?>" aria-expanded="true" aria-controls="<?= 'faq_collapse_' . $faq['id']; ?>">
                                 <?= $faq['faq_title']; ?>
                             </button>
                         </h5>
