c63a29ee6a1d7d7b8ea73446a54f712ccfb1ace7
andybao <wenyu.bao@gmail.com>
2018-05-21 16:23:23 -0400

    update faq demo

--- home/app.js
+++ home/app.js
@@ -1,12 +1,16 @@
-import getPrefix from './lib/getPrefix';
 import Promise from 'bluebird';
 import preStyles from 'raw-loader!./prestyles.css';
 import {default as writeChar, writeSimpleChar} from "./lib/writeChar";
 let styleText = [0].map(function(i) { return require('raw-loader!./styles' + i + '.css'); });
-import workText from 'raw-loader!./info.txt';
-import testText from 'raw-loader!./testinfo.txt';
+import txt_0 from 'raw-loader!./txt/0.txt';
+import txt_1 from 'raw-loader!./txt/1.txt';
+import txt_2 from 'raw-loader!./txt/2.txt';
+import txt_3 from 'raw-loader!./txt/3.txt';
+import txt_4 from 'raw-loader!./txt/4.txt';
+import txt_5 from 'raw-loader!./txt/5.txt';
+import txt_6 from 'raw-loader!./txt/6.txt';
 let style, styleEl, workEl, pgpEl, skipAnimationEl, pauseEl, textbtn;
-let browserPrefix;
+let displaySpeed = 0;
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -17,44 +21,90 @@ document.addEventListener("DOMContentLoaded", function() {
 
     startAnimation();
 
-    $("#test_btn").click(function () {
-       testAnimation();
+    $("#f_c_btn").click(function () {
+       faqClientAnimation();
+    });
+    
+    $("#f_a_create_btn").click(function () {
+        faqAdminCreateAnimation();
+    });
+
+    $("#f_a_edit_btn").click(function () {
+       faqAdminEditAnimation();
+    });
+
+    $("#f_a_delete_btn").click(function () {
+        faqAdminDeleteAnimation();
     });
 
 });
 
-
-
 async function startAnimation() {
     try {
         //await writeTo(styleEl, styleText[0], 0, 16, true, 1);
-        await writeTo(workEl, workText, 0, 16, false, 1);
-        $("#card").show("slow");
-
-
+        await writeTo(workEl, txt_0, 0, displaySpeed, false, 1);
+        await $("#card").show("slow");
+        await Promise.delay(2000);
+        await writeTo(workEl, txt_1, 0, displaySpeed, false, 1);
+        await Promise.delay(2000);
+        await writeTo(workEl, txt_2, 0, displaySpeed, false, 1);
+        await $("#f_c_btn").prop('disabled', false);
     }
         // Flow control straight from the ghettos of Milwaukee
     catch(e) {
-        if (e.message === "SKIP IT") {
-
-        } else {
-            throw e;
-        }
+        throw e;
     }
 }
 
-async function testAnimation() {
+async function faqClientAnimation() {
     try {
         //await writeTo(styleEl, styleText[0], 0, 16, true, 1);
-        await writeTo(workEl, testText, 0, 16, false, 1);
+        await writeTo(workEl, txt_3, 0, displaySpeed, false, 1);
+        await $("#f_c_btn").prop('disabled', true);
+        await $("#f_a_create_btn").prop('disabled', false);
     }
         // Flow control straight from the ghettos of Milwaukee
     catch(e) {
-        if (e.message === "SKIP IT") {
+        throw e;
+    }
+}
 
-        } else {
-            throw e;
-        }
+async function faqAdminCreateAnimation() {
+    try {
+        //await writeTo(styleEl, styleText[0], 0, 16, true, 1);
+        await writeTo(workEl, txt_4, 0, displaySpeed, false, 1);
+        await $("#f_a_create_btn").prop('disabled', true);
+        await $("#f_a_edit_btn").prop('disabled', false);
+    }
+        // Flow control straight from the ghettos of Milwaukee
+    catch(e) {
+        throw e;
+    }
+}
+
+async function faqAdminEditAnimation() {
+    try {
+        //await writeTo(styleEl, styleText[0], 0, 16, true, 1);
+        await writeTo(workEl, txt_5, 0, displaySpeed, false, 1);
+        await $("#f_a_edit_btn").prop('disabled', true);
+        await $("#f_a_delete_btn").prop('disabled', false);
+    }
+        // Flow control straight from the ghettos of Milwaukee
+    catch(e) {
+        throw e;
+    }
+}
+
+async function faqAdminDeleteAnimation() {
+    try {
+        //await writeTo(styleEl, styleText[0], 0, 16, true, 1);
+        await writeTo(workEl, txt_6, 0, displaySpeed, false, 1);
+        await $("#f_a_delete_btn").prop('disabled', true);
+        //await $("#f_a_delete_btn").prop('disabled', false);
+    }
+        // Flow control straight from the ghettos of Milwaukee
+    catch(e) {
+        throw e;
     }
 }
 
